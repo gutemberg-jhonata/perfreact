e9d6baed78c251f7cbc869f6808efe97f03819a8
Gutemberg <gutemberg.jhonata@gmail.com>
2022-02-28 19:13:17 -0300

    Formating data while searching

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -5,18 +5,14 @@ type SearchResultProps = {
     results: Array<{
         id: number,
         price: number,
+        priceFormatted: string,
         title: string,
     }>,
+    totalPrice: number,
     onAddToWishList: (id: number) => void,
 }
 
-export function SearchResults({ results, onAddToWishList }: SearchResultProps) {
-    const totalPrice = useMemo(() => {
-        return results.reduce((total, product) => {
-            return total + product.price;
-        }, 0)
-    }, [results])
-
+export function SearchResults({ results, totalPrice, onAddToWishList }: SearchResultProps) {
     return (
         <div>
             <h2>{totalPrice}</h2>
