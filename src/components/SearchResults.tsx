411a537536f050577f8cb1c32753d706dea1e899
Gutemberg <gutemberg.jhonata@gmail.com>
2022-02-28 18:36:32 -0300

    Calculation total price in SearchResult Component with useMemo

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import { ProductItem } from "./ProductItem";
 
 type SearchResultProps = {
@@ -9,8 +10,16 @@ type SearchResultProps = {
 }
 
 export function SearchResults({ results }: SearchResultProps) {
+    const totalPrice = useMemo(() => {
+        return results.reduce((total, product) => {
+            return total + product.price;
+        }, 0)
+    }, [results])
+
     return (
         <div>
+            <h2>{totalPrice}</h2>
+
             {results.map(product => {
                 return (
                     <ProductItem key={product.id} product={product} />
