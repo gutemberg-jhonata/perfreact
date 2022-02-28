8070fe94875dba45a695463b6682926b33a75db5
Gutemberg <gutemberg.jhonata@gmail.com>
2022-02-28 10:34:08 -0300

    Init project

---
+++ src/components/SearchResults.tsx
@@ -0,0 +1,21 @@
+import { ProductItem } from "./ProductItem";
+
+type SearchResultProps = {
+    results: Array<{
+        id: number,
+        price: number,
+        title: string
+    }>
+}
+
+export function SearchResults({ results }: SearchResultProps) {
+    return (
+        <div>
+            {results.map(product => {
+                return (
+                    <ProductItem key={product.id} product={product} />
+                )
+            })}
+        </div>
+    );
+}
